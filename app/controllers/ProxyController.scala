397296b4cba7292cc7ba0ea8f765f6cc41b08f88
arnelandwehr <arne.landwehr@innoq.com>
2016-03-03 19:31:25 +0100

    add some rudimentary failure handling

--- app/controllers/ProxyController.scala
+++ app/controllers/ProxyController.scala
@@ -10,20 +10,27 @@ import services.ResultTransformer.transformResult
 import services.ServerGateway.forwardRequestToServer
 
 import scala.concurrent.Future
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
+import play.api.Logger
 
 class ProxyController @Inject()(ws: WSClient)(configuration: Configuration) extends Controller {
 
   implicit val context = play.api.libs.concurrent.Execution.Implicits.defaultContext
 
   def proxy(requestPath: String) = Action.async(parse.tolerantText) { originalRequest =>
-    val result = for {
+    val result: Try[Future[Result]] = for {
       forwardingRequest <- mapToForwardingRequest(originalRequest, requestPath, configuration)
       serverResponse <- Try(forwardRequestToServer(forwardingRequest, ws))
       transformedResult <- transformResult(serverResponse)
     } yield transformedResult
-    result.getOrElse(Future(InternalServerError))
+    result match {
+      case Failure(ex) =>
+        Logger.error(ex.getMessage, ex)
+        Future(InternalServerError(ex.getMessage))
+      case Success(result) =>
+        result
+    }
   }
 
 }
